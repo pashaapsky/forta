1ad098f684ba6b02fd75dcd292bcf6c8102b9f87
pashaapsky <ap.sky@yandex.ru>
2021-02-16 09:37:38 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import React, {useEffect, useState} from 'react';
-import {Routes} from './routes'
-import {useKeycloak} from "@react-keycloak/web";
+import React from 'react';
 
 function App() {
-    const [auth, setAuth] = useState({
-        keycloak: null,
-        authenticated: false,
-    });
-
-    const {keycloak, initialized} = useKeycloak();
-
-    useEffect(() => {
-        console.log(keycloak);
-        // keycloak
-        //     .init({ onLoad: 'login-required' })
-        //     .then((authenticated) => {
-        //         console.log(authenticated);
-        //
-        //         setAuth({
-        //             keycloak: keycloak,
-        //             authenticated: true
-        //         })
-        //
-        //         // localStorage.setItem('AT', keycloak.token);
-        //     })
-    }, []);
-
-
-
     return (
         <div className="App">
-            <Routes />
+            <div className="wrapper">
+                <div className="sidebar">
+
+                </div>
+
+                <main className="content">
+                    <div className="table">
+                        <div className="row">
+                            <div className="item">ID</div>
+                            <div className="item">NAME</div>
+                            <div className="item">FIELD1</div>
+                            <div className="item">OPTION2</div>
+                            <div className="item">EXTRA FIELD</div>
+                        </div>
+
+                        <div className="row">
+                            <div className="item">{Math.floor(Math.random() * 150)}</div>
+                            <div className="item">Lorem ipsum dolor.</div>
+                            <div className="item">Lorem ipsum.</div>
+                            <div className="item">Lorem ipsum dolor sit.</div>
+                            <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
+                        </div>
+
+                        <div className="row">
+                            <div className="item">{Math.floor(Math.random() * 150)}</div>
+                            <div className="item">Lorem ipsum dolor.</div>
+                            <div className="item">Lorem ipsum.</div>
+                            <div className="item">Lorem ipsum dolor sit.</div>
+                            <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
+                        </div>
+                    </div>
+                </main>
+            </div>
         </div>
-    );
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {ReactKeycloakProvider} from "@react-keycloak/web";
 import "./scss/styles.scss"
-import keycloak from "./keycloak";
 
-ReactDOM.render(
-    <ReactKeycloakProvider authClient={keycloak}>
-        <App />
-    </ReactKeycloakProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
